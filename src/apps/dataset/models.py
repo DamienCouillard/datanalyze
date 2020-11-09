777d6009bb0d1a0fb3bc27f63bfab88874ee7be2
DamienCouillard <damien.couillard@hotmail.com>
2020-11-09 14:18:55 +0100

    index as id for datasets

--- src/apps/dataset/models.py
+++ src/apps/dataset/models.py
@@ -4,7 +4,7 @@ from apps.utils.variables import EXTRACTORS_LIST
 
 class Dataset(models.Model):
     label = models.CharField(max_length=100)
-    index = models.CharField(max_length=100)
+    index = models.CharField(max_length=100, primary_key=True)
     description = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=1000, default='')
