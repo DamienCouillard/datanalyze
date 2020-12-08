fa51f9828fcfb537247454598dd58189cd5f5885
DamienCouillard <damien.couillard@hotmail.com>
2020-12-08 10:59:56 +0100

    UI Design

--- src/apps/dataset/models.py
+++ src/apps/dataset/models.py
@@ -5,7 +5,7 @@ from apps.utils.variables import EXTRACTORS_LIST
 class Dataset(models.Model):
     label = models.CharField(max_length=100)
     index = models.CharField(max_length=100, primary_key=True)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=10000)
     created_at = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=1000, default='')
     source_type = models.CharField(max_length=100, choices= EXTRACTORS_LIST)
--- web/src/components/Sidebar/Sidebar.jsx
+++ web/src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
-import "../../style/Dashboard.css";
 
 const Side = (props) => {
   return (
     <>
-      <Nav
-        className="col-md-12 d-none d-md-block bg-light sidebar"
-        activeKey="/"
-      >
-        <div className="sidebar-sticky"></div>
-        <Nav.Item>
-          <Nav.Link href="/">Datasets</Nav.Link>
+      <Nav className="col-md-12 d-none d-md-block sidebar" activeKey="/">
+        <h1 className="sidebar-title">Datanalyze</h1>
+        <Nav.Item key="dataset">
+          <Nav.Link className="sidebar-item" href="/">
+            Datasets
+          </Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/tables">Tables</Nav.Link>
+        <Nav.Item key="tables">
+          <Nav.Link className="sidebar-item" href="/tables">
+            Tables
+          </Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/analyze">Analyze</Nav.Link>
+        <Nav.Item key="analyze">
+          <Nav.Link className="sidebar-item" href="/analyze">
+            Analyze
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     </>
